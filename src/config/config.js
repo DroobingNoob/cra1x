cb66d52c0d68321d689eb510e43c8980c4aacdb3
aayushdasgupta <aayushdasgupta0408@gmail.com>
2025-11-25 08:01:23 +0530

    Changed BASE_URL

--- src/config/config.js
+++ src/config/config.js
@@ -2,4 +2,4 @@
 
 // export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://mitzi-purehearted-delila.ngrok-free.dev" || "http://localhost:5000";
 
-export const BASE_URL =  "https://mitzi-purehearted-delila.ngrok-free.dev"
+export const BASE_URL =  "https://associate-quick-arrested-surplus.trycloudflare.com"
