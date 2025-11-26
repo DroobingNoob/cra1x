26820d63aba52f8b20869b5c7e212130db65e12d
aayushdasgupta <aayushdasgupta0408@gmail.com>
2025-11-26 20:34:19 +0530

    fixed api

--- src/config/config.js
+++ src/config/config.js
@@ -2,4 +2,5 @@
 
 // export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://mitzi-purehearted-delila.ngrok-free.dev" || "http://localhost:5000";
 
-export const BASE_URL =  "http://srv1145973.hstgr.cloud"
+// export const BASE_URL =  "http://srv1145973.hstgr.cloud"
+export const BASE_URL = "http://srv1145973.hstgr.cloud/api";
