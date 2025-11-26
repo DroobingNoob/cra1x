508de939ad4bf703ba553292174f278ac4994566
aayushdasgupta <aayushdasgupta0408@gmail.com>
2025-11-26 10:49:36 +0530

    changed url for backend

--- src/config/config.js
+++ src/config/config.js
@@ -2,4 +2,4 @@
 
 // export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://mitzi-purehearted-delila.ngrok-free.dev" || "http://localhost:5000";
 
-export const BASE_URL =  "https://associate-quick-arrested-surplus.trycloudflare.com"
+export const BASE_URL =  "http://72.61.227.209:5000/"
