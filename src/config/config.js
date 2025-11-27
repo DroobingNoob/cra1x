ddff7ed3233f5c62a1042e73b9a2f62e07ee34a2
aayushdasgupta <aayushdasgupta0408@gmail.com>
2025-11-27 06:57:19 +0530

    changed http to https

--- src/config/config.js
+++ src/config/config.js
@@ -3,4 +3,4 @@
 // export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://mitzi-purehearted-delila.ngrok-free.dev" || "http://localhost:5000";
 
 // export const BASE_URL =  "http://srv1145973.hstgr.cloud"
-export const BASE_URL = "http://srv1145973.hstgr.cloud/api";
+export const BASE_URL = "https://srv1145973.hstgr.cloud/api";
